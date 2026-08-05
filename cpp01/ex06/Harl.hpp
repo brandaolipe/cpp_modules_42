cbb9c837be780cc2b32a41b6d2ecf811058a5842
Felipe <felipebrandaochaves@gmail.com>
2026-08-05 18:31:14 -0300

    feat: ex06

---
+++ cpp01/ex06/Harl.hpp
@@ -0,0 +1,22 @@
+#ifndef HARL_HPP
+# define HARL_HPP
+
+#include <iostream>
+#include <string>
+
+class	Harl
+{
+	public:
+		Harl();
+		~Harl();
+		void	harlFilter(std::string level);
+
+	private:
+		void	debug(void);
+		void	info(void);
+		void	warning(void);
+		void	error(void);
+		int		getLevel(std::string level);
+};
+
+#endif
